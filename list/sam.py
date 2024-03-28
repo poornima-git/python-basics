6e7ae6829b25ec69ffacf93fa20ead977de39d04
Poornima V <poornimaviswanathan2002@gmail.com>
2024-03-28 08:36:46 +0530

    sample programs

--- list/sam.py
+++ list/sam.py
@@ -1,21 +1,36 @@
 
 
-# creating an empty list
-my_list = []
+# # creating an empty list
+# my_list = []
  
-# number of elements as input
-n = int(input("Enter number of elements : "))
+# # number of elements as input
+# n = int(input("Enter number of elements : "))
  
-# iterating till the range
-for i in range(0, n):
-    ele = int(input())
-    # adding the element
-    my_list.append(ele)
+# # iterating till the range
+# for i in range(0, n):
+#     ele = int(input())
+#     # adding the element
+#     my_list.append(ele)
 
-my_list = input("enter a lists(comma:").split(',')
-my_list = [i.strip() for i in my_list]
-guk = list(my_list)
-list_pos = int(input("enter the position"))
-my_list = my_list[:list_pos - 1] + my_list[list_pos:]
-print(list(my_list))
-print(type(list_pos))
+# my_list = input("enter a lists(comma:").split(',')
+# my_list = [i.strip() for i in my_list]
+# guk = list(my_list)
+# list_pos = int(input("enter the position"))
+# my_list = my_list[:list_pos - 1] + my_list[list_pos:]
+# print(list(my_list))
+# print(type(list_pos))
+
+
+
+# num_1 = int(input("enter an integer:"))
+# num_2 = int(input("enter an integer:"))
+# a = num_1 + num_2
+# b = num_1 * num_2
+# if (a < 1000):
+#     print(a)
+# elif(b < 1000):
+#     print(b)
+# else:
+#     print("the sum is greater than 1000")
+
+
