aa51c0d75de015c40c2a37b0ee24f941e8078b17
Poornima V <poornimaviswanathan2002@gmail.com>
2024-04-04 16:27:18 +0530

    loops sample

---
+++ loops/while.py
@@ -0,0 +1,39 @@
+# print  1 to 100
+# i = 0
+# while i < 100:
+#     i += 1
+#     print(i)
+
+# print 100 to 1
+# i = 101
+# while i >= 1:
+#     i -= 1
+    # print(i)
+
+#multiplication table of a number n
+
+# num = int(input("enter a number:-"))
+# i = 1
+# while i <= 10:
+#     print(num,'*',i, '=', num*i)
+#     i += 1
+
+#print the squares of same number till 10
+
+# i = 1
+# if i == i:
+#     while i <= 10:
+#         print(i * i)
+#         i += 1
+
+#print the list of number [1,4,9,16,25,36,49,64,81,100]
+
+num = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+index = 0
+while index < len(num):
+    print(num[index])
+    index += 1
+                 
+
+
+
